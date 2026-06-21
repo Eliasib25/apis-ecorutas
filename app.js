e88ef501be18fef9f4af1e54611dc0b7f86964da
Eliasib25 <eliasib.benitez@cecar.edu.co>
2026-06-20 22:59:07 -0500

    new cors route

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://apis-ecorutas-083211715d4f.herokuapp.com/api'],
   credentials: true
 }));
 app.use(express.json());
